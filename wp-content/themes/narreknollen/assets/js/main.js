5e79dce4108fe34a06fffd8c5d9794787ec4c314
jasperevers <jasperevers@hotmail.nl>
2025-11-21 15:53:19 +0100

    migrated SCSS and JavaScript files to new directory structure in `assets` and updated import paths accordingly

---
+++ wp-content/themes/narreknollen/assets/js/main.js
@@ -0,0 +1,10 @@
+import "../scss/style.scss"
+
+// Our components / classes
+import MobileMenu from "./components/MobileMenu"
+
+import CustomizerHandler from "./components/customizer-handler";
+
+// Instantiate a new object using our components/classes
+var mobileMenu = new MobileMenu()
+var customizerHandler = new CustomizerHandler();
