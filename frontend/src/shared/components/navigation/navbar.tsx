888aead2c35787341afc5cdb28d6fc555f73f56a
jasperevers <jasperevers@hotmail.nl>
2026-04-29 20:09:43 +0200

    added mobile variand for the page

--- frontend/src/shared/components/navigation/navbar.tsx
+++ frontend/src/shared/components/navigation/navbar.tsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
-import {useEffect, useState} from "react";
 
 export default function Navbar() {
     const [scrolled, setScrolled] = useState(false);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         const onScroll = () => setScrolled(window.scrollY > 20);
         window.addEventListener("scroll", onScroll);
         return () => window.removeEventListener("scroll", onScroll);
     }, []);
+
+    const toggleMenu = () => setOpen((prev) => !prev);
+
+    const closeMenu = () => setOpen(false);
 
     return (
         <nav className={`navbar ${scrolled ? "navbar--scrolled" : ""}`}>
@@ -19,9 +24,15 @@ export default function Navbar() {
                     Narre Knollen
                 </div>
 
-                <div className="navbar__links">
-                    <NavLink to="/" className="navlink">Home</NavLink>
-                    <NavLink to="/events" className="navlink">Events</NavLink>
+                {/* hamburger button */}
+                <button className="navbar__toggle" onClick={toggleMenu}>
+                    ☰
+                </button>
+
+                {/* menu */}
+                <div className={`navbar__links ${open ? "is-open" : ""}`}>
+                    <NavLink to="/" onClick={closeMenu}>Home</NavLink>
+                    <NavLink to="/events" onClick={closeMenu}>Events</NavLink>
                 </div>
 
             </div>
