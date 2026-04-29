43d52ca2eb3197544529afd29802b260c8dff48a
jasperevers <jasperevers@hotmail.nl>
2026-04-29 17:50:26 +0200

    added eventpreview component and

---
+++ frontend/src/components/events/EventsPreview.tsx
@@ -0,0 +1,41 @@
+import {Link} from "react-router-dom";
+
+export default function EventsPreview() {
+    const events = [
+        {id: 1, title: "Optocht", date: "10 feb"},
+        {id: 2, title: "Prinsenbal", date: "15 feb"},
+        {id: 3, title: "Kleintje carnaval", date: "15 feb"},
+    ];
+
+    return (
+        <section className="section container">
+            <h2>Evenementen</h2>
+
+            <div className="grid">
+                {events.map((event) => (
+                    <Link
+                        key={event.id}
+                        to={`/events/${event.id}`}
+                        className="card card--link"
+                    >
+                        <h3>{event.title}</h3>
+
+                        <p className="card__location">
+                            Duivenzaaltje, Oostergracht 46 3763 ZL Soest
+                        </p>
+
+                        <p className="card__date">{event.date}</p>
+
+                        <p className="card__summary">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </p>
+
+                        <span className="card__link">
+              Meer info →
+            </span>
+                    </Link>
+                ))}
+            </div>
+        </section>
+    );
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Hero from "../components/hero/Hero.tsx";
-import EventsPreview from "../components/EventsPreview";
+import EventsPreview from "../components/events/EventsPreview";
 import AboutSection from "../components/AboutSection";
 import CurrentHighnessSection from "../components/highness/CurrentHighnessSection.tsx";
 
