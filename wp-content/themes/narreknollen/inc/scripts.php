eebd10ce10f25de3044b0473df191fef337c6f09
jasperevers <jasper.evers@student.hu.nl>
2026-06-20 13:02:38 +0200

    Refactored header and front-page structure, updated SCSS themes with improved layouts and utility classes, redesigned navbar, hero section, and buttons for better responsiveness.

--- wp-content/themes/narreknollen/inc/scripts.php
+++ wp-content/themes/narreknollen/inc/scripts.php
@@ -9,7 +9,7 @@
  */
 function narreknollen_files() {
     wp_enqueue_script('main-narreknollen_old-js', get_theme_file_uri('/build/main.js'), array('jquery'), '1.0', true);
-    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('narreknollen_main_styles', get_theme_file_uri('/build/style-index.css'));
 }
